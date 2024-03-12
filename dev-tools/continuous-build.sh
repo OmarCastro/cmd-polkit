4c7d889ebd1b2f4729a921c71ed835e8ef279d42
OmarCastro <omar.castro.360@gmail.com>
2024-03-12 21:02:19 +0000

    build: test and report test coverage

--- dev-tools/continuous-build.sh
+++ dev-tools/continuous-build.sh
@@ -4,13 +4,17 @@ cd ..
 
 PROJECT_DIR=$(pwd)
 
-rm -rf build
-mkdir build
-cd build
-meson --prefix=/usr ..
-cd ..
-while inotifywait -e close_write $PROJECT_DIR/src $PROJECT_DIR/test; do 
-    cd $PROJECT_DIR/build
-    ninja
+build_project() {
+    cd $PROJECT_DIR/build-test
+    meson build
     meson test
+    ninja coverage-html
+}
+
+rm -rf build-test
+meson setup build-test -Db_coverage=true --reconfigure
+build_project
+
+while inotifywait -e close_write $PROJECT_DIR/src $PROJECT_DIR/test; do 
+    build_project
 done
