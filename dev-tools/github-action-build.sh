1bcbf1084754dbfc9df6afd1435a2621409b694d
OmarCastro <omar.castro.360@gmail.com>
2024-03-15 03:15:58 +0000

    build: remove --reconfigure for github actions

--- dev-tools/github-action-build.sh
+++ dev-tools/github-action-build.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
-meson setup build-test -Db_coverage=true --reconfigure
+meson setup build-test -Db_coverage=true
 meson build -C build-test
 meson test -C build-test
 ninja coverage-html -C build-test
