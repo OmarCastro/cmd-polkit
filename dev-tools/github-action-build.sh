77ae40e885d25d3a0a5d276d3bd46f82789e4858
OmarCastro <omar.castro.360@gmail.com>
2024-03-15 18:24:57 +0000

    build: use conditional chain

--- dev-tools/github-action-build.sh
+++ dev-tools/github-action-build.sh
@@ -2,8 +2,8 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
-meson setup build-test -Db_coverage=true
-meson compile -C build-test
-meson test -C build-test
-ninja coverage -C build-test
+meson setup build-test -Db_coverage=true && \
+meson compile -C build-test && \
+meson test -C build-test && \
+ninja coverage -C build-test && \
 dev-tools/run docs
