e29778c7f3bdec46d3fa8a993bc85a1b0e60ef5f
OmarCastro <omar.castro.360@gmail.com>
2024-03-15 18:36:30 +0000

    build: use early exit

--- dev-tools/github-action-build.sh
+++ dev-tools/github-action-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
-meson setup build-test -Db_coverage=true && \
-meson compile -C build-test && \
-meson test -C build-test && \
-ninja coverage -C build-test && \
+meson setup build-test -Db_coverage=true
+meson compile -C build-test
+meson test -C build-test
+ninja coverage -C build-test
 dev-tools/run docs
