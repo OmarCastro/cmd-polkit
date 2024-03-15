7411afa068e9f84e6bf2d8b20798f77aa3c350ee
OmarCastro <omar.castro.360@gmail.com>
2024-03-15 03:16:21 +0000

    build: run all coverage reports

--- dev-tools/github-action-build.sh
+++ dev-tools/github-action-build.sh
@@ -5,5 +5,5 @@ cd ..
 meson setup build-test -Db_coverage=true
 meson build -C build-test
 meson test -C build-test
-ninja coverage-html -C build-test
+ninja coverage -C build-test
 dev-tools/run docs
