76bac11582394932a80d46ddf95cf73636873bbd
OmarCastro <omar.castro.360@gmail.com>
2024-03-15 03:20:33 +0000

    build: fix meson action command

--- dev-tools/github-action-build.sh
+++ dev-tools/github-action-build.sh
@@ -3,7 +3,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
 meson setup build-test -Db_coverage=true
-meson build -C build-test
+meson compile -C build-test
 meson test -C build-test
 ninja coverage -C build-test
 dev-tools/run docs
