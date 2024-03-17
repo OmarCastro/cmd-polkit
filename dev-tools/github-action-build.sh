eb83e95ac5948befbe77dbabecdb8d21fc06732f
OmarCastro <omar.castro.360@gmail.com>
2024-03-17 02:58:13 +0000

    build: only exit if critical step fails

--- dev-tools/github-action-build.sh
+++ dev-tools/github-action-build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-set -e
-
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
-meson setup build-test -Db_coverage=true
-meson compile -C build-test
+meson setup build-test -Db_coverage=true || exit 1
+meson compile -C build-test || exit 1
 meson test -C build-test
 ninja coverage -C build-test
 dev-tools/run docs
