f69e0e225eb16616a180fc11d57dd6a2dedca562
OmarCastro <omar.castro.360@gmail.com>
2024-03-18 19:56:24 +0000

    src: remove unused json_node_get_boolean_or_else functions

--- src/json-glib.extension.c
+++ src/json-glib.extension.c
@@ -14,12 +14,3 @@ const gchar * json_node_get_string_or_else(JsonNode * node, const gchar * else_v
            json_node_get_value_type(node) == G_TYPE_STRING ?
            json_node_get_string(node) : else_value;
 }
-
-
-gboolean json_object_get_boolean_member_or_else(JsonObject * node, const gchar * member, gboolean else_value){
-    return json_node_get_boolean_or_else(json_object_get_member(node, member), else_value);
-}
-
-const gchar * json_object_get_string_member_or_else(JsonObject * node, const gchar * member, const gchar * else_value){
-    return json_node_get_string_or_else(json_object_get_member(node, member), else_value);
-}
--- src/json-glib.extension.h
+++ src/json-glib.extension.h
@@ -5,11 +5,7 @@
 
 #include <json-glib/json-glib.h>
 
-gboolean json_node_get_boolean_or_else(JsonNode * node, gboolean else_value);
-
 const gchar * json_node_get_string_or_else(JsonNode * node, const gchar * else_value);
-
-gboolean json_object_get_boolean_member_or_else(JsonObject * node, const gchar * member, gboolean else_value);
 
 const gchar * json_object_get_string_member_or_else(JsonObject * node, const gchar * member, const gchar * else_value);
 
