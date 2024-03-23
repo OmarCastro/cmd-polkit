2d2d42d0e3f35a8281309385549a958b9c12be2b
OmarCastro <omar.castro.360@gmail.com>
2024-03-23 17:39:04 +0000

    test: support older version of glibc

--- test/polkit.mock.c
+++ test/polkit.mock.c
@@ -1,28 +1,30 @@
 #include "polkit.mock.h"
 
-
-static void request(gpointer session){
-          g_signal_emit_by_name (session, "request", "Password:", FALSE);
+static int request(gpointer session){
+  g_signal_emit_by_name (session, "request", "Password:", FALSE);
+	return G_SOURCE_REMOVE;
 }
 
-
-static void complete_success(gpointer session){
+static int complete_success(gpointer session){
   g_signal_emit_by_name (session, "completed", TRUE);
+  return G_SOURCE_REMOVE;
+}
+
+static int complete_error(gpointer session){
+  g_signal_emit_by_name (session, "completed", FALSE);
+  return G_SOURCE_REMOVE;
 }
 
 void mock_polkit_agent_session_initiate (PolkitAgentSession *session){
-    g_idle_add_once(request, session);
-
+  g_idle_add(request, session);
 }
 
-void mock_polkit_agent_session_cancel (PolkitAgentSession *session)
-{
-    g_signal_emit_by_name (session, "completed", FALSE);
+void mock_polkit_agent_session_cancel (PolkitAgentSession *session){
+  g_idle_add(complete_error, session);
 }
 
-void mock_polkit_agent_session_response (PolkitAgentSession *session, const gchar *response)
-{
-        g_idle_add_once(complete_success, session);
+void mock_polkit_agent_session_response (PolkitAgentSession *session, const gchar *response){
+  g_idle_add(complete_success, session);
 }
 
 
