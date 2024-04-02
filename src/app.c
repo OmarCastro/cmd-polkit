a9882d4d1709c83ae5f2395ac0f3e8d46fac0b5b
OmarCastro <omar.castro.360@gmail.com>
2024-04-02 21:55:03 +0100

    refactor: reuse cmd_line_argv

--- src/app.c
+++ src/app.c
@@ -8,22 +8,18 @@
 
 static AuthHandlingMode handling_mode;
 static char *cmd_line = NULL;
+static char ** cmd_line_argv = NULL;
 static int static_argc;
 static char ** static_argv;
 static bool isInitialized = false;
 
-const char*  app__get_cmd_line(){
+const char* app__get_cmd_line(){
   return cmd_line;
 }
 
-typedef struct 
-{
-	int argc;
-	char ** argv;
-	const char* command_line;
-	AuthHandlingMode handling_mode;
-} Application;
-
+char** app__get_cmd_line_argv(){
+  return cmd_line_argv;
+}
 
 AuthHandlingMode app__get_auth_handling_mode(){
   return handling_mode;
@@ -47,7 +43,7 @@ int app__init(int argc, char *argv[]){
   struct gengetopt_args_info ai;
   if (cmdline_parser(argc, argv, &ai) != 0) {
     cmdline_parser_print_help();
-    exit(1);
+    return 1;
   }
 
   bool runInSerie = ai.serial_given;
@@ -72,16 +68,13 @@ int app__init(int argc, char *argv[]){
 
   cmd_line = ai.command_arg;
 
-  char **cmd_argv = NULL;
   GError* error = NULL;
 
-  if ( !g_shell_parse_argv ( ai.command_arg, NULL, &cmd_argv, &error ) ){
+  if ( !g_shell_parse_argv ( ai.command_arg, NULL, &cmd_line_argv, &error ) ){
       fprintf(stderr, "Unable to parse cmdline options: %s\n", error->message);
       g_error_free ( error );
       return 1;
   }
-
-  g_strfreev(cmd_argv);
 
   if(runInSerie && runInParallel){
     log__fail_cmdline__either_parallel_or_series();
