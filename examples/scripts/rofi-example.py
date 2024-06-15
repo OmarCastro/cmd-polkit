08cdbfc99c8e4d4757f734fca27018646a7f81c1
Omar Castro <omar.castro.360@gmail.com>
2024-06-15 18:56:52 -0400

    build: update example to use "polkit action" field
    see #8

--- examples/scripts/rofi-example.py
+++ examples/scripts/rofi-example.py
@@ -9,7 +9,7 @@ rofi_message = message
 prompt = "password: "
 
 def open_rofi():
-    cmd = ['rofi', '-password', '-dmenu', '-p', prompt, '-mesg', rofi_message ]
+    cmd = ['rofi', '-password', '-dmenu', '-markup', '-no-fixed-num-lines', '-p', prompt, '-mesg', rofi_message ]
     p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     out, err = p.communicate();
     if out == b'':
@@ -30,6 +30,13 @@ for line in sys.stdin:
         if "message" in jsonObj:
             message = jsonObj["message"]
             rofi_message = message
+        if "polkit action" in jsonObj and jsonObj["polkit action"] != None:
+            polkit_action = jsonObj["polkit action"]
+            rofi_message = rofi_message + "\n\n"
+            if "description" in polkit_action: rofi_message = rofi_message + "\n<b>    Action:</b> " + polkit_action["description"]
+            if "id"          in polkit_action: rofi_message = rofi_message + "\n<b> Action ID:</b> " + polkit_action["id"] 
+            if "vendor name" in polkit_action: rofi_message = rofi_message + "\n<b>    Vendor:</b> " + polkit_action["vendor name"] 
+            if "vendor url"  in polkit_action: rofi_message = rofi_message + "\n<b>Vendor URL:</b> " + polkit_action["vendor url"]
         open_rofi()
     #elif jsonObj["action"] == "authorization response":
     #    if "authorized" in jsonObj:
