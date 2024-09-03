457d76e1690600f4e35733620d7276ac0c49930d
OmarCastro <omar.castro.360@gmail.com>
2024-09-03 17:20:31 +0100

    fix: remove --reconfigure on github actions
    see #9

--- dev-tools/github-action-build-alphine-musl.sh
+++ dev-tools/github-action-build-alphine-musl.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
-meson setup build-alphine-musl -Db_coverage=true --reconfigure --warnlevel 2 || exit 1
+meson setup build-alphine-musl -Db_coverage=true --warnlevel 2 || exit 1
 meson compile -C build-test || exit 1
 meson test --wrap='valgrind --leak-check=full' -C build-alphine-musl
 ninja coverage -C build-alphine-musl
