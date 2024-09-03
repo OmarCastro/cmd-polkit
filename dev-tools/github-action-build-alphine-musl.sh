94b77232b0c013273b2afe2cdcc100cba66c80aa
OmarCastro <omar.castro.360@gmail.com>
2024-09-03 17:31:28 +0100

    fix: seems like alphine linux does not have bash, welp, lets try sh
    context: #9

--- dev-tools/github-action-build-alphine-musl.sh
+++ dev-tools/github-action-build-alphine-musl.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE[0]}")"
-cd ..
+#!/usr/bin/env sh
+DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+cd "$DIR/.."
 
 meson setup build-alphine-musl -Db_coverage=true --warnlevel 2 || exit 1
-meson compile -C build-test || exit 1
+meson compile -C build-alphine-musl || exit 1
 meson test --wrap='valgrind --leak-check=full' -C build-alphine-musl
 ninja coverage -C build-alphine-musl
